fecff74e538839a9ae21e11d0e9027af8551da97
Divyam Thakur <56884990+explooo@users.noreply.github.com>
2023-10-25 23:01:47 +0530

    ADdded scrolling carousel

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key});
@@ -7,9 +8,96 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       
+      
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            CarouselSlider(
+              items: [
+                Container(
+                  margin: const EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.red,
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Item 1',
+                      style: const TextStyle(fontSize: 24.0, color: Colors.white),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.blue,
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Item 2',
+                      style: TextStyle(fontSize: 24.0, color: Colors.white),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.green,
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Item 3',
+                      style: TextStyle(fontSize: 24.0, color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+              options: CarouselOptions(
+                height: 200.0,
+                enlargeCenterPage: true,
+                autoPlay: true,
+                aspectRatio: 16 / 9,
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enableInfiniteScroll: true,
+                autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                viewportFraction: 0.8,
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Card(
+                    child: ListTile(
+                      title: const Text('Explore Events'),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Card(
+                    child: ListTile(
+                      title: const Text('Explore Courses'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: 20,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: ListTile(
+                    title: Text('Card ${index + 1}'),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-     
-      
-    
