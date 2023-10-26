bff1c653c4cf420bccd3c2f38239b9d4b6287d4e
Divyam Thakur <56884990+explooo@users.noreply.github.com>
2023-10-27 00:11:16 +0530

    added scaffold

--- lib/pages/events.dart
+++ lib/pages/events.dart
@@ -1,15 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:unipedia/pages/home.dart';
+
+
 class eventsScreen extends StatelessWidget {
   const eventsScreen({Key? key, required this.title}) : super(key: key);
   final String title;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       body: Center(
-        child: TextButton(
-          onPressed: () {},
-          child: const Text('Go Back'),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePage()),
+                );
+              },
+              child: Container(
+                width: 100.0,
+                height: 100.0,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Container 1',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 10.0),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePage()),
+                );
+              },
+              child: Container(
+                width: 100.0,
+                height: 100.0,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Container 2',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key});
@@ -7,96 +7,82 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      
       body: SingleChildScrollView(
         child: Column(
           children: [
-            CarouselSlider(
-              items: [
-                Container(
-                  margin: const EdgeInsets.all(5.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Colors.red,
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Item 1',
-                      style: const TextStyle(fontSize: 24.0, color: Colors.white),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(5.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Colors.blue,
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'Item 2',
-                      style: TextStyle(fontSize: 24.0, color: Colors.white),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(5.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Colors.green,
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'Item 3',
-                      style: TextStyle(fontSize: 24.0, color: Colors.white),
-                    ),
-                  ),
-                ),
-              ],
-              options: CarouselOptions(
-                height: 200.0,
-                enlargeCenterPage: true,
-                autoPlay: true,
-                aspectRatio: 16 / 9,
-                autoPlayCurve: Curves.fastOutSlowIn,
-                enableInfiniteScroll: true,
-                autoPlayAnimationDuration: const Duration(milliseconds: 800),
-                viewportFraction: 0.8,
-              ),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Card(
-                    child: ListTile(
-                      title: const Text('Explore Events'),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Card(
-                    child: ListTile(
-                      title: const Text('Explore Courses'),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: 20,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  child: ListTile(
-                    title: Text('Card ${index + 1}'),
-                  ),
-                );
-              },
-            ),
+            topCarousel(),
+            StudyToolsText(),
           ],
         ),
+      ),
+    );
+  }
+
+  Padding StudyToolsText() {
+    return const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text(
+              'Study Tools',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          );
+  }
+
+  CarouselSlider topCarousel() {
+    return CarouselSlider(
+      items: [
+        Container(
+          margin: const EdgeInsets.all(5.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: Colors.red,
+          ),
+          child: const Center(
+            child: Text(
+              'Item 1',
+              style: TextStyle(fontSize: 24.0, color: Colors.white),
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.all(5.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: Colors.blue,
+          ),
+          child: const Center(
+            child: Text(
+              'Item 2',
+              style: TextStyle(fontSize: 24.0, color: Colors.white),
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.all(5.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: Colors.green,
+          ),
+          child: const Center(
+            child: Text(
+              'Item 3',
+              style: TextStyle(fontSize: 24.0, color: Colors.white),
+            ),
+          ),
+        ),
+      ],
+      options: CarouselOptions(
+        height: 200.0,
+        enlargeCenterPage: true,
+        autoPlay: true,
+        aspectRatio: 16 / 9,
+        autoPlayCurve: Curves.fastOutSlowIn,
+        enableInfiniteScroll: true,
+        autoPlayAnimationDuration: const Duration(milliseconds: 800),
+        viewportFraction: 0.8,
       ),
     );
   }
